b9cd4fd7dfd909fb27e6b4b81795b7e6919f67ed
Shayne999 <shaynejuniorn@gmail.com>
2024-05-25 14:45:57 +0200

    Models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
+
+
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    username = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    bio = models.TextField(max_length=500, blank=True)
+    profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
+    location = models.CharField(max_length=30, blank=True)
+
+    def __str__(self):
+        return self.user.email
