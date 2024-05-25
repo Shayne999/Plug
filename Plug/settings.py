3927428b592cebb6855048bc077fe66843426c32
Shayne999 <shaynejuniorn@gmail.com>
2024-05-25 17:41:33 +0200

    Database Setup

--- Plug/settings.py
+++ Plug/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Plug.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Plug_Database',
+        'USER': 'postgres',
+        'PASSWORD': 'Iamjmagnus!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from .models import Profile
 
 # Create your views here.
 def index(request):
@@ -18,7 +19,7 @@ def login(request):
            return redirect('home.html')
         else:
            messages.info(request, 'Invalid Credentials')
-           return redirect('/')
+           #return redirect('/')
 
     else:
         return render(request, 'index.html')
@@ -31,20 +32,27 @@ def sign_up(request):
         email = request.POST['email']
         password = request.POST['password']
         password2 = request.POST['password2']
-        
+
         if password == password2:
-            if User.objects.filter(username=username).exists():
-                messages.info(request, 'Username Taken')
-                return redirect('sign_up')
-            elif User.objects.filter(email=email).exists():
+            if User.objects.filter(email=email).exists():
                 messages.info(request, 'Email Taken')
+                return redirect('sign_up')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
                 return redirect('sign_up')
             else:
                 user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
                 user.save()
-                return redirect('index')
+            
+                #profile object for new user
+                user_models = User.object.get(username=username)
+                new_profile = Profile.objects.create(user=user_models, id_user=user_models.id)
+                new_profile.save()
+                return redirect('login')
+
         else:
-            return messages.info(request, 'Password Not Matching')
+            messages.info(request, 'Password Not Matching')
+            return redirect('sign_up')
     else:
         return render(request, 'sign_up.html')
 
