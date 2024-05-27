c6e44847be2a1e145a82eea31ba031c5f232a921
Shayne999 <shaynejuniorn@gmail.com>
2024-05-27 13:17:46 +0200

    Feed View Fix

--- feed/urls.py
+++ feed/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.feed_view, name='feed'),
-    path('upload/', views.feed_view, name='upload'),
+    path('upload/', views.upload, name='upload'),
 
 ]
