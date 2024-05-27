d4b66efaf8120e2da1aadb78ba9ed7d23456230f
Shayne999 <shaynejuniorn@gmail.com>
2024-05-27 13:53:29 +0200

    Posts Working

--- feed/models.py
+++ feed/models.py
@@ -16,4 +16,4 @@ class Post(models.Model):
     likes_count = models.IntegerField(default=0)
     created_at = models.DateTimeField(default=timezone.now)
     def __str__(self):
-        return self.username
+        return f"{self.user.username}'s Post"
--- feed/views.py
+++ feed/views.py
@@ -10,4 +10,22 @@ def feed_view(request):
     return render(request, 'feed.html')
 
 def upload(request):
+    if request.method == 'POST':
+        user = request.user
+        image = request.FILES.get('image')
+        video = request.FILES.get('video')
+        caption = request.POST['caption']
+
+        #media check
+        if not image and not video:
+            messages.info(request, 'Upload Failed')
+            return redirect('feed')
+
+        new_post = Post.objects.create(user=user, image=image, video=video, caption=caption)
+        new_post.save()
+    else:
+        messages.info(request, 'Upload Failed')
+        return redirect('feed')
+
+    
     return render(request, 'feed.html')
