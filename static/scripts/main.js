bafd911ff535807ec0bfc37ddfab2c5665591495
Shayne999 <shaynejuniorn@gmail.com>
2024-05-31 11:39:14 +0200

    Navigation Bar Improved

---
+++ static/scripts/main.js
@@ -0,0 +1,25 @@
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+// const currentLocation = location.pathname;
+// const menuItem = document.querySelectorAll('.nav-link');
+// const menuLength = menuItem.length;
+// for (let i = 0; i < menuLength; i++) {
+//     if (menuItem[i].getAttribute("href") === currentLocation) {
+//     menuItem[i].classList.add('active');
+//     }
+// }
+// });
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const currentLocation = location.pathname;
+    const menuItem = document.querySelectorAll('.nav-link');
+    const menuLength = menuItem.length;
+    for (let i = 0; i < menuLength; i++) {
+        if (menuItem[i].getAttribute("href") === currentLocation) {
+            menuItem[i].classList.add('active');
+            menuItem[i].style.backgroundColor = '#007bff'; // Apply inline style
+        }
+    }
+});
+
+
