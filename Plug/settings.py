4069af22c50460ad94c64f9a525eadadc491cf97
Shayne999 <shaynejuniorn@gmail.com>
2024-06-07 17:40:13 +0200

    Yaml File

--- Plug/settings.py
+++ Plug/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from django.core.wsgi import get_wsgi_application
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-porii=kscnb05iui@4xf#g#p_*!3scphbq7&0b+j$fb9c(t323'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
